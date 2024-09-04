371732efd0e53593954a8f78af86a8921db340af
Mhmod <mahmoud97787@gmail.com>
2024-09-04 22:59:21 +0300

    add AppColors

---
+++ lib/utils/constats.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const primaryColor = Color(0xFF274460);
+  static const secondaryColor = Color(0xFF006487);
+  static const backgroundLightColor = Color(0xFFFAFAFA);
+}

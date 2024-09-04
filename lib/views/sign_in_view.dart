58bcc471c8983365daa870f33ff6cf36ddd1db3e
Mhmod <mahmoud97787@gmail.com>
2024-09-04 22:53:30 +0300

    add assets

---
+++ lib/views/sign_in_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SignInView extends StatelessWidget {
+  const SignInView({super.key});
+  static const route = 'sing_in_view';
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
